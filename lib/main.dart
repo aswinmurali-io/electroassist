d17424f3f893dce19540512888c06fab276f8119
rithviknishad <rithvikn2001@gmail.com>
2021-07-06 19:35:22 +0530

    improves entry point and support theme updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,53 @@
 import 'package:electroassist/routes/dashboard.dart';
+import 'package:electroassist/shared/themes.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ElectroAssist());
 
-class ElectroAssist extends StatelessWidget {
+/// Is a [Widget] and also contains core properties and information about this
+/// app as static declarations.
+class ElectroAssist extends StatefulWidget {
+  /// The name of the app. Defaults to: `Electro Assist`.
+  static const appName = 'Electro Assist';
+
+  createState() => _ElectroAssistState();
+
+  /// Returns the instance of `State<ElectroAssist>` in the widget tree of
+  /// [context].
+  ///
+  /// **Usage example**
+  /// ```dart
+  /// // Updates the theme with [customTheme].
+  /// ElectroAssist.of(context).theme = customTheme;
+  /// ```
+  static _ElectroAssistState? of(BuildContext context) =>
+      context.findAncestorStateOfType<_ElectroAssistState>();
+}
+
+class _ElectroAssistState extends State<ElectroAssist> {
+  /// The selected theme of the app.
+  ///
+  /// Defaults to [ElectroAssistThemes.defaultTheme].
+  ///
+  /// See also:
+  /// * [ElectroAssistThemes]
+  ThemeData _selectedTheme = ElectroAssistThemes.defaultTheme;
+
+  /// The current theme this app uses.
+  ThemeData get theme => _selectedTheme;
+
+  /// Updates the theme this app uses with the specified [value].
+  set theme(ThemeData value) => setState(() => _selectedTheme = value);
+
   @override
   build(context) {
     return MaterialApp(
-      title: 'Electro Assist',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-          textTheme: TextTheme(
-            headline6: TextStyle(
-              color: Colors.blueGrey.shade400,
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
-          ),
-        ),
-      ),
+      title: ElectroAssist.appName,
+      theme: _selectedTheme,
       home: Dashboard(),
+
+      // Disables the debug banner during debug builds.
+      debugShowCheckedModeBanner: false,
     );
   }
 }
