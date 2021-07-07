f7db5a4d80b36845fb003469c64e45a3e26b984a
rithviknishad <rithvikn2001@gmail.com>
2021-07-07 12:29:23 +0530

    migrate impl. to existing

--- lib/routes/calculators/resistor_color_codes.dart
+++ lib/routes/calculators/resistor_color_codes.dart
@@ -1,39 +1,5 @@
 import 'package:electroassist/main.dart';
-import 'package:electroassist/shared/components/module.dart';
-import 'package:electroassist/shared/widgets/module_tile.dart';
 import 'package:flutter/material.dart';
-
-class ResistorColorCodes extends Module {
-  @override
-  get name => 'Resistor Color Codes';
-
-  @override
-  get description =>
-      'Calculate resistance based on the color code given in the resistor';
-
-  @override
-  get page => ResistorColorCodePage();
-
-  @override
-  get style => ModuleTileStyle(
-        avatorColor: Color(0xff8f94fb),
-        image: Icon(
-          Icons.ac_unit,
-          color: Colors.white,
-        ),
-        style: TextStyle(
-          color: Colors.white,
-        ),
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color(0xff4e54c8),
-            Color(0xff8f94fb),
-          ],
-        ),
-      );
-}
 
 class ResistorColorCodePage extends StatefulWidget {
   const ResistorColorCodePage({Key? key}) : super(key: key);
