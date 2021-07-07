bf31b7fe117943c2be3b8a63e34da11fbf1a0988
rithviknishad <rithvikn2001@gmail.com>
2021-07-07 11:54:12 +0530

    adds Resistor Color Code Module

---
+++ lib/modules/resistor_color_code.dart
@@ -0,0 +1,9 @@
+import 'package:electroassist/shared/components/module.dart';
+
+class ResistorColorCode extends Module {
+  @override
+  String get name => 'Resistor Color Code';
+
+  @override
+  String get description => '';
+}
