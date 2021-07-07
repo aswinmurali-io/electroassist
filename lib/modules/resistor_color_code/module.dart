659c612afd8ac230348bc0f2d627108cef0cb0e6
rithviknishad <rithvikn2001@gmail.com>
2021-07-07 12:33:08 +0530

    migrate module definitions to new structure

--- lib/modules/resistor_color_code/module.dart
+++ lib/modules/resistor_color_code/module.dart
@@ -1,4 +1,4 @@
-import 'package:electroassist/routes/calculators/resistor_color_codes.dart';
+import 'package:electroassist/modules/resistor_color_code/route.dart';
 import 'package:electroassist/shared/components/module.dart';
 import 'package:electroassist/shared/widgets/module_tile.dart';
 import 'package:flutter/material.dart';
