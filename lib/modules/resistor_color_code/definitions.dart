e979e7867791a9ccdbba2e1fe16c874de979d2c2
Aswin Murali <aswinmurali.co@gmail.com>
2021-07-11 10:16:31 +0530

    adds types

--- lib/modules/resistor_color_code/definitions.dart
+++ lib/modules/resistor_color_code/definitions.dart
@@ -243,6 +243,13 @@ abstract class Resistor {
     return value * multiplier;
   }
 
+  static const resistorTypes = <Type>[
+    ThreeBandResistor,
+    FourBandResistor,
+    FiveBandResistor,
+    SixBandResistor,
+  ];
+
   Resistor._({
     required this.digit1Band,
     required this.digit2Band,
@@ -274,6 +281,9 @@ abstract class Resistor {
 }
 
 class ThreeBandResistor extends Resistor {
+
+  
+
   ThreeBandResistor._(List<ResistorBandColor> bands)
       : assert(bands.length == 3),
         super._(
