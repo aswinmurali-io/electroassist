40a78e069584f159de278757fcf97f40a30cebfb
rithviknishad <rithvikn2001@gmail.com>
2021-07-11 12:31:10 +0530

    breaking changes to resistor color code definitions

--- lib/modules/resistor_color_code/definitions.dart
+++ lib/modules/resistor_color_code/definitions.dart
@@ -1,358 +1,187 @@
-import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
+import 'dart:math';
 
-enum ResistorBandType {
-  ThreeBandResistor,
-  FourBandResistor,
-  FiveBandResistor,
-  SizBandResistor,
-}
+import 'package:flutter/material.dart';
+
+enum ResistorBandType { threeBand, fourBand, fiveBand, sixBand }
 
 class ResistorColorCodeViewModel extends ChangeNotifier {
-  ResistorBandType _bandType = ResistorBandType.FourBandResistor;
+  ResistorBandType _bandType = ResistorBandType.fourBand;
 
   ResistorBandType get bandType => _bandType;
-}
 
-/// This class contains properties associated to the color for the color band
-/// of a resistor.
-///
-/// Contains static declarations of pre-defined colors that can be used to
-/// access its properties.
-@immutable
-@sealed
-class ResistorBandColor {
-  /// Represents the [Color] of the band.
-  final Color color;
+  set bandType(ResistorBandType value) {
+    if (_bandType != value) {
+      _bandType = value;
+      notifyListeners();
+    }
+  }
 
-  /// The digit value of the band.
-  /// Can be null, if this color band has no digit defined.
-  final int? asDigit;
+  int get numberOfDigitBands {
+    switch (_bandType) {
+      case ResistorBandType.threeBand:
+      case ResistorBandType.fourBand:
+        return 2;
+      case ResistorBandType.fiveBand:
+      case ResistorBandType.sixBand:
+        return 3;
+    }
+  }
 
-  /// The multiplier value of the band.
-  ///
-  /// **Example**
-  /// ```dart
-  /// // Signifies multiplier is 1e-2
-  /// ResistorColorBand.Silver.asMultiplier == 0.01;
-  /// ```
-  final double asMultiplier;
+  bool get hasToleranceBand {
+    switch (_bandType) {
+      case ResistorBandType.threeBand:
+        return false;
+      case ResistorBandType.fourBand:
+      case ResistorBandType.fiveBand:
+      case ResistorBandType.sixBand:
+        return true;
+    }
+  }
 
-  /// The tolerance percentage that is represented by this color band.
-  /// Can be null, if this color band has no tolerance percentage property
-  /// defined.
-  ///
-  /// ```dart
-  /// // Signifies tolerance is +/- 4.0%
-  /// ResistorColorBand.Yellow.asTolerancePercentage == 4;
-  /// ```
-  final double? asTolerancePercentage;
+  bool get hasTemperatureCoefficientBand {
+    switch (_bandType) {
+      case ResistorBandType.threeBand:
+      case ResistorBandType.fourBand:
+      case ResistorBandType.fiveBand:
+        return false;
+      case ResistorBandType.sixBand:
+        return true;
+    }
+  }
 
-  ////
-  final int? asTemperatureCoefficient;
+  static final _random = Random();
 
-  const ResistorBandColor._({
-    required this.color,
-    required this.asDigit,
-    required this.asMultiplier,
-    required this.asTolerancePercentage,
-    required this.asTemperatureCoefficient,
-  });
+  static num randomDigitBand() =>
+      digitBandValues.elementAt(_random.nextInt(digitBandColors.length));
 
-  static const Black = ResistorBandColor._(
-      color: Colors.black,
-      asDigit: 0,
-      asMultiplier: 1,
-      asTolerancePercentage: null,
-      asTemperatureCoefficient: 250);
+  static num randomMultiplierBand() => multiplierBandColors.keys
+      .elementAt(_random.nextInt(multiplierBandColors.length));
 
-  static const Brown = ResistorBandColor._(
-      color: Colors.brown,
-      asDigit: 1,
-      asMultiplier: 1e1,
-      asTolerancePercentage: 1,
-      asTemperatureCoefficient: 100);
+  static num randomToleranceBand() => toleranceBandColors.keys
+      .elementAt(_random.nextInt(toleranceBandColors.length));
 
-  static const Red = ResistorBandColor._(
-      color: Colors.red,
-      asDigit: 2,
-      asMultiplier: 1e2,
-      asTolerancePercentage: 2,
-      asTemperatureCoefficient: 50);
+  static num randomTemperatureCoefficientBand() => tempCoeffBandColors.keys
+      .elementAt(_random.nextInt(tempCoeffBandColors.length));
 
-  static const Orange = ResistorBandColor._(
-      color: Colors.orange,
-      asDigit: 3,
-      asMultiplier: 1e3,
-      asTolerancePercentage: 3,
-      asTemperatureCoefficient: 15);
+  num _digit1 = randomDigitBand();
+  num _digit2 = randomDigitBand();
+  num _digit3 = randomDigitBand();
+  num _multiplier = randomMultiplierBand();
+  num _tolerance = randomToleranceBand();
+  num _temperatureCoefficient = randomTemperatureCoefficientBand();
 
-  static const Yellow = ResistorBandColor._(
-      color: Colors.yellow,
-      asDigit: 4,
-      asMultiplier: 1e4,
-      asTolerancePercentage: 4,
-      asTemperatureCoefficient: 25);
+  num get digit1 => _digit1;
+  num get digit2 => _digit2;
+  num get digit3 => _digit3;
+  num get multiplier => _multiplier;
+  num get tolerance => _tolerance;
+  num get temperatureCoefficient => _temperatureCoefficient;
 
-  static const Green = ResistorBandColor._(
-      color: Colors.green,
-      asDigit: 5,
-      asMultiplier: 1e5,
-      asTolerancePercentage: 0.5,
-      asTemperatureCoefficient: 20);
+  set digit1(num value) {
+    if (digit1 != value) {
+      _digit1 = value;
+      notifyListeners();
+    }
+  }
 
-  static const Blue = ResistorBandColor._(
-      color: Colors.blue,
-      asDigit: 6,
-      asMultiplier: 1e6,
-      asTolerancePercentage: 0.25,
-      asTemperatureCoefficient: 10);
+  set digit2(num value) {
+    if (digit2 != value) {
+      _digit2 = value;
+      notifyListeners();
+    }
+  }
 
-  static const Violet = ResistorBandColor._(
-      color: Colors.purple,
-      asDigit: 7,
-      asMultiplier: 1e7,
-      asTolerancePercentage: 0.1,
-      asTemperatureCoefficient: 5);
+  set digit3(num value) {
+    if (digit3 != value) {
+      _digit3 = value;
+      notifyListeners();
+    }
+  }
 
-  static const Grey = ResistorBandColor._(
-      color: Colors.grey,
-      asDigit: 8,
-      asMultiplier: 1e8,
-      asTolerancePercentage: 0.05,
-      asTemperatureCoefficient: 1);
+  set multiplier(num value) {
+    if (multiplier != value) {
+      _multiplier = value;
+      notifyListeners();
+    }
+  }
 
-  static const White = ResistorBandColor._(
-      color: Colors.white,
-      asDigit: 8,
-      asMultiplier: 1e9,
-      asTolerancePercentage: null,
-      asTemperatureCoefficient: null);
+  set tolerance(num value) {
+    if (tolerance != value) {
+      _tolerance = value;
+      notifyListeners();
+    }
+  }
 
-  static const Gold = ResistorBandColor._(
-      color: Colors.yellowAccent,
-      asDigit: null,
-      asMultiplier: 1e-1,
-      asTolerancePercentage: 5,
-      asTemperatureCoefficient: null);
+  set temperatureCoefficientBand(num value) {
+    if (temperatureCoefficient != value) {
+      _temperatureCoefficient = value;
+      notifyListeners();
+    }
+  }
 
-  static const Silver = ResistorBandColor._(
-      color: Colors.grey,
-      asDigit: null,
-      asMultiplier: 1e-2,
-      asTolerancePercentage: 10,
-      asTemperatureCoefficient: null);
-
-  static const None = ResistorBandColor._(
-      color: Colors.transparent,
-      asDigit: null,
-      asMultiplier: 0,
-      asTolerancePercentage: 20,
-      asTemperatureCoefficient: null);
-
-  /// Contains all valid digit bands.
-  static const digitBands = [
-    Black,
-    Brown,
-    Red,
-    Orange,
-    Yellow,
-    Green,
-    Blue,
-    Violet,
-    Grey,
-    White,
-  ];
-
-  /// Contains all valid multiplier bands.
-  static const multiplierBands = [
-    Silver,
-    Gold,
-    Black,
-    Brown,
-    Red,
-    Orange,
-    Yellow,
-    Green,
-    Blue,
-    Violet,
-    Grey,
-    White,
-  ];
-
-  /// Contains all valid tolerance bands.
-  static const toleranceBands = [
-    Grey,
-    Violet,
-    Blue,
-    Green,
-    Brown,
-    Red,
-    Orange,
-    Yellow,
-    Gold,
-    Silver,
-    None,
-  ];
-
-  /// The default tolerance band.
-  static const defaultToleranceBand = Gold;
-
-  /// Contains all valid temperature coefficient bands.
-  static const temperatureCoefficientBands = [
-    Black,
-    Brown,
-    Red,
-    Orange,
-    Yellow,
-    Green,
-    Blue,
-    Violet,
-    Grey,
-  ];
-}
-
-/// An abstract interface of a resistor.
-///
-/// **Example:**
-/// ```dart
-/// // To auto detect resistor band type and construct appropriate instance:
-/// var r1 = Resistor(bands);
-///
-/// // or manually create of a desired type:
-/// var r2 = ThreeBandResistor(bands);
-/// ```
-///
-/// **See also:**
-/// * [ThreeBandResistor]
-/// * [FourBandResistor]
-/// * [FiveBandResistor]
-/// * [SixBandResistor]
-@immutable
-abstract class Resistor {
-  final ResistorBandColor digit1Band, digit2Band, multiplierBand;
-
-  final ResistorBandColor? digit3Band, toleranceBand, temperatureCoeffBand;
-
-  int get digit1 => digit1Band.asDigit!;
-
-  int get digit2 => digit2Band.asDigit!;
-
-  int? get digit3 => digit3Band?.asDigit;
-
-  double get multiplier => multiplierBand.asMultiplier;
-
-  double? get tolerancePercentage => toleranceBand?.asTolerancePercentage;
-
-  int? get temperatureCoefficient =>
-      temperatureCoeffBand?.asTemperatureCoefficient;
-
-  /// The resistance value in `Ohms`.
-  double get resistance {
+  num get resistance {
     var value = digit1 * 10 + digit2;
 
-    if (digit3 != null) value = value * 10 + digit3!;
+    if (numberOfDigitBands > 2) {
+      value = value * 10 + digit3;
+    }
 
     return value * multiplier;
   }
 
-  static const resistorTypes = <Type>[
-    ThreeBandResistor,
-    FourBandResistor,
-    FiveBandResistor,
-    SixBandResistor,
-  ];
+  static final digitBandColors = {
+    0: Colors.black,
+    1: Colors.brown,
+    2: Colors.red,
+    3: Colors.orange,
+    4: Colors.yellow,
+    5: Colors.green,
+    6: Colors.blue,
+    7: Colors.purple,
+    8: Colors.grey,
+    9: Colors.white,
+  };
 
-  Resistor._({
-    required this.digit1Band,
-    required this.digit2Band,
-    required this.digit3Band,
-    required this.multiplierBand,
-    required this.toleranceBand,
-    required this.temperatureCoeffBand,
-  });
+  static Iterable<int> get digitBandValues => digitBandColors.keys;
 
-  factory Resistor(List<ResistorBandColor> bands) {
-    assert(3 <= bands.length && bands.length <= 6,
-        "3-band, 4-band, 5-band and 6-band resistors are only valid. Received ${bands.length}-band.");
+  static final multiplierBandColors = {
+    1e0: Colors.black,
+    1e1: Colors.brown,
+    1e2: Colors.red,
+    1e3: Colors.orange,
+    1e4: Colors.yellow,
+    1e5: Colors.green,
+    1e6: Colors.blue,
+    1e7: Colors.purple,
+    1e8: Colors.grey,
+    1e9: Colors.white,
+    1e-1: Colors.amber,
+    1e-2: Colors.grey.shade300,
+  };
 
-    switch (bands.length) {
-      case 3:
-        return ThreeBandResistor._(bands);
-      case 4:
-        return FourBandResistor._(bands);
-      case 5:
-        return FiveBandResistor._(bands);
-      case 6:
-        return SixBandResistor._(bands);
-    }
+  static final toleranceBandColors = {
+    1: Colors.brown,
+    2: Colors.red,
+    3: Colors.orange,
+    4: Colors.yellow,
+    .5: Colors.green,
+    .25: Colors.blue,
+    .1: Colors.purple,
+    .05: Colors.grey,
+    5: Colors.amber,
+    10: Colors.grey.shade300,
+    20: Colors.transparent,
+  };
 
-    throw Exception(
-      'Failed to generate appropriate resistor of type ${bands.length}-band Resistor',
-    );
-  }
-}
-
-class ThreeBandResistor extends Resistor {
-
-  
-
-  ThreeBandResistor._(List<ResistorBandColor> bands)
-      : assert(bands.length == 3),
-        super._(
-          // associated bands
-          digit1Band: bands[0],
-          digit2Band: bands[1],
-          multiplierBand: bands[2],
-
-          // non-associated bands
-          digit3Band: null,
-          toleranceBand: null,
-          temperatureCoeffBand: null,
-        );
-}
-
-class FourBandResistor extends Resistor {
-  FourBandResistor._(List<ResistorBandColor> bands)
-      : assert(bands.length == 4),
-        super._(
-          // associated bands
-          digit1Band: bands[0],
-          digit2Band: bands[1],
-          multiplierBand: bands[2],
-          toleranceBand: bands[3],
-
-          // non-associated bands
-          digit3Band: null,
-          temperatureCoeffBand: null,
-        );
-}
-
-class FiveBandResistor extends Resistor {
-  FiveBandResistor._(List<ResistorBandColor> bands)
-      : assert(bands.length == 5),
-        super._(
-          // associated bands
-          digit1Band: bands[0],
-          digit2Band: bands[1],
-          digit3Band: bands[2],
-          multiplierBand: bands[3],
-          toleranceBand: bands[4],
-
-          // non-associated bands
-          temperatureCoeffBand: null,
-        );
-}
-
-class SixBandResistor extends Resistor {
-  SixBandResistor._(List<ResistorBandColor> bands)
-      : assert(bands.length == 6),
-        super._(
-          digit1Band: bands[0],
-          digit2Band: bands[1],
-          digit3Band: bands[2],
-          multiplierBand: bands[3],
-          toleranceBand: bands[4],
-          temperatureCoeffBand: bands[5],
-        );
+  static final tempCoeffBandColors = {
+    250: Colors.black,
+    100: Colors.brown,
+    50: Colors.red,
+    15: Colors.orange,
+    25: Colors.yellow,
+    20: Colors.green,
+    10: Colors.blue,
+    5: Colors.purple,
+    1: Colors.grey,
+  };
 }
