2061c3ee4d04166a15f7de48e6a3e830907483a9
Aswin Murali <aswinmurali.co@gmail.com>
2021-07-11 15:52:57 +0530

    adds new updated app bar in settings

--- lib/routes/settings.dart
+++ lib/routes/settings.dart
@@ -1,4 +1,7 @@
+import 'package:animations/animations.dart';
 import 'package:electroassist/main.dart';
+import 'package:electroassist/routes/dashboard.dart';
+import 'package:electroassist/shared/widgets/gradient_floating_action_button.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -26,30 +29,58 @@ class _SettingsPageState extends State<SettingsPage> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: Text("Settings"),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 60, 0, 10),
+                child: Text(
+                  "Settings",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    color: Colors.blueGrey,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  'Dark Theme',
+                  style: TextStyle(
+                    color: Colors.blueGrey.shade700,
+                  ),
+                ),
+                subtitle: Text(
+                  'Enable to disable dark mode.',
+                  style: TextStyle(
+                    color: Colors.blueGrey.shade400,
+                  ),
+                ),
+                trailing: Switch(
+                  value: _isDarkMode,
+                  onChanged: (value) => setState(() => _isDarkMode = value),
+                ),
+              ),
+            ],
+          ),
         ),
-        body: ListView(
-          children: [
-            ListTile(
-              title: Text(
-                'Dark Theme',
-                style: TextStyle(
-                  color: Colors.blueGrey.shade700,
-                ),
-              ),
-              subtitle: Text(
-                'Enable to disable dark mode.',
-                style: TextStyle(
-                  color: Colors.blueGrey.shade400,
-                ),
-              ),
-              trailing: Switch(
-                value: _isDarkMode,
-                onChanged: (value) => setState(() => _isDarkMode = value),
-              ),
-            ),
-          ],
+        bottomNavigationBar: OpenContainer(
+          closedElevation: 0,
+          openColor: Colors.transparent,
+          closedColor: Colors.transparent,
+          openElevation: 0,
+          openBuilder: (context, _) => Dashboard(),
+          closedShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          closedBuilder: (context, action) {
+            return GradientFloatingActionButton(
+              child: Icon(Icons.home),
+              onPressed: action,
+              tooltip: "Go back to home",
+            );
+          },
         ),
       ),
     );
