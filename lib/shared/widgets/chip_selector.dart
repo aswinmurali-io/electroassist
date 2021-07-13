57874f8448c283bcba767954b17db867d2ce0ac3
rithviknishad <rithvikn2001@gmail.com>
2021-07-13 12:35:21 +0530

    chip selector modified

--- lib/shared/widgets/chip_selector.dart
+++ lib/shared/widgets/chip_selector.dart
@@ -1,92 +1,64 @@
-import 'package:bouncing_widget/bouncing_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animator/flutter_animator.dart';
 
-class ChipSelector<T> extends StatefulWidget {
-  final List<String> tabs;
+class ChipSelectorItem<T> {
+  const ChipSelectorItem({
+    required this.value,
+    required this.itemBuilder,
+  });
 
-  final int Function(int index) onSelected;
+  final T value;
 
-  ChipSelector({
-    Key? key,
-    required this.onSelected,
-    required this.tabs,
-  }) : super(key: key);
-
-  @override
-  createState() => _ChipSelectorState();
+  final Widget Function(
+    BuildContext context,
+    bool isSelected,
+    VoidCallback selectCallback,
+  ) itemBuilder;
 }
 
-class _ChipSelectorState extends State<ChipSelector> {
-  int _selectedIndex = 0;
+class ChipSelector<T> extends StatefulWidget {
+  ChipSelector({
+    Key? key,
+    required this.initialValue,
+    required this.items,
+    this.onChanged,
+  })  : assert(items.isNotEmpty, "Atleast one item shall be present"),
+        super(key: key);
+
+  final List<ChipSelectorItem<T>> items;
+
+  final T initialValue;
+
+  final ValueChanged<T>? onChanged;
+
+  @override
+  createState() => _ChipSelectorState<T>();
+}
+
+class _ChipSelectorState<T> extends State<ChipSelector<T>> {
+  late T value;
+
+  @override
+  initState() {
+    super.initState();
+    value = widget.initialValue;
+  }
 
   @override
   build(context) {
-    return GridView.count(
-      crossAxisCount: 3,
-      childAspectRatio: 2.4,
-      children: List.generate(widget.tabs.length, (index) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            width: 130,
-            child: BouncingWidget(
-              onPressed: () => setState(() => _selectedIndex = index),
-              child: ZoomIn(
-                preferences: AnimationPreferences(
-                  duration: Duration(milliseconds: 100),
-                  offset: Duration(milliseconds: 200 + index * 50),
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: _selectedIndex == index
-                          ? [Colors.red, Colors.yellow]
-                          : [Colors.blueGrey.shade50, Colors.white],
-                    ),
-                    borderRadius: BorderRadius.circular(25),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.blueGrey.shade50,
-                        offset: Offset(0, 3),
-                        blurRadius: 5.0,
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(8, 5, 8, 5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(
-                          Icons.label,
-                          color: _selectedIndex == index
-                              ? Colors.white
-                              : Colors.blueGrey.shade400,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          '${widget.tabs[index]}',
-                          style: TextStyle(
-                            color: _selectedIndex == index
-                                ? Colors.white
-                                : Colors.blueGrey.shade400,
-                            fontWeight: _selectedIndex == index
-                                ? FontWeight.bold
-                                : FontWeight.normal,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
+    return Wrap(
+      spacing: 10,
+      runSpacing: 10,
+      children: [
+        for (final item in widget.items)
+          item.itemBuilder(
+            context,
+            item.value == value,
+            () {
+              setState(() => value = item.value);
+              widget.onChanged?.call(value);
+            },
           ),
-        );
-      }),
+      ],
     );
   }
 }
