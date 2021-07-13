17eb91f888742d0d9f0a40f884afb7e99a41bcf3
rithviknishad <rithvikn2001@gmail.com>
2021-07-13 14:25:06 +0530

    improve resistor color code

--- lib/modules/resistor_color_code/route.dart
+++ lib/modules/resistor_color_code/route.dart
@@ -1,39 +1,15 @@
-import 'package:electroassist/modules/resistor_color_code/module.dart';
-import 'package:electroassist/shared/widgets/gradients/fab.dart';
-import 'package:electroassist/shared/widgets/resistor_tabbar.dart';
+import 'package:electroassist/modules/resistor_color_code/definitions.dart';
+import 'package:electroassist/shared/widgets/chip_selector.dart';
+import 'package:electroassist/shared/widgets/navigator_pop_fab.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ResistorBandState {
-  ResistorBandState._();
+class ResistorColorCodePage extends StatelessWidget {
+  const ResistorColorCodePage({Key? key}) : super(key: key);
 
-  static final instance = ResistorBandState._();
-
-  int? bandTypeIndex;
-
-  int? firstBandColorIndex;
-
-  int? secondBandColorIndex;
-
-  int? thirdBandColorIndex;
-
-  int? fourthBandColorIndex;
-
-  int? fivethBandColorIndex;
-
-  int? sixthBandColorIndex;
-}
-
-class ResistorColorCodePage extends StatefulWidget {
   @override
-  createState() => _ResistorColorCodePageState();
-}
-
-class _ResistorColorCodePageState extends State<ResistorColorCodePage> {
-  @override
-  build(context) {
-    final resistorState = ResistorBandState.instance;
-    final theme = Theme.of(context);
+  Widget build(BuildContext context) {
     return DecoratedBox(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -45,81 +21,64 @@ class _ResistorColorCodePageState extends State<ResistorColorCodePage> {
           ],
         ),
       ),
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 70, 0, 10),
-                    child: Text(
-                      "Resistor Calculator",
-                      style: theme.textTheme.headline1,
-                    ),
-                  ),
-                  Text("RESISTOR IMAGE HERE"),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                    child: Text(
-                      "Band selection",
-                      style: TextStyle(
-                        color: Colors.blueGrey,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 110,
-                    child: ChipSelector(
-                      onSelected: (index) =>
-                          resistorState.bandTypeIndex = index,
-                      tabs: [
-                        "3 Band",
-                        "4 Band",
-                        "5 Band",
-                        "6 Band",
-                      ],
-                    ),
-                  ), //SizedBox(height: 20),
-                  SizedBox(
-                    height: 200,
-                    child: ChipSelector(
-                      onSelected: (index) =>
-                          resistorState.bandTypeIndex = index,
-                      tabs: [
-                        "3 Band",
-                        "4 Band",
-                        "5 Band",
-                        "6 Band",
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: ChangeNotifierProvider.value(
+              value: ResistorColorCodeViewModel(),
+              builder: (_, __) => _ResistorColorCodeView(),
             ),
-          ),
-          floatingActionButton: GradientFloatingActionButton(
-            onPressed: Navigator.of(context).pop,
-            child: Icon(Icons.chevron_left_sharp),
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xffD16BA5),
-                Color(0xff86A8E7),
-                Colors.tealAccent,
-              ],
-            ),
-            tooltip: 'Go back to home',
           ),
         ),
+        floatingActionButton: NavigatorPopFloatingActionButton(
+          tooltip: 'Back to dashboard',
+        ),
       ),
+    );
+  }
+}
+
+class _ResistorColorCodeView extends StatelessWidget {
+  const _ResistorColorCodeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 70, 0, 10),
+          child: Text("Resistor Calculator"),
+        ),
+        Text("RESISTOR IMAGE HERE"),
+        SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+          child: Text(
+            "Band selection",
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        ChipSelector<ResistorBandType>(
+          initialValue: ResistorBandType.fourBand,
+          items: [
+            for (final type in ResistorBandType.values)
+              ChipSelectorItem(
+                value: type,
+                itemBuilder: (context, isSelected, selectItem) {
+                  return ElevatedButton(
+                    child: Text('$type'),
+                    onPressed: selectItem,
+                  );
+                },
+              ),
+          ],
+        ),
+      ],
     );
   }
 }
