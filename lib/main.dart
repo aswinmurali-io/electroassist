3119290e1294b2bae018cd8f02b030e814420d24
Rithvik Nishad <rithvikn2001@gmail.com>
2021-08-02 10:53:21 +0530

    move theme to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:electroassist/routes/dashboard.dart';
-import 'package:electroassist/shared/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 void main() => runApp(ElectroAssist());
 
@@ -58,4 +58,37 @@ class _ElectroAssistState extends State<ElectroAssist> {
       debugShowCheckedModeBanner: false,
     );
   }
+}
+
+/// Contains all the in-built themes of this app. This class shall not be
+/// inherited nor be constructed, as it shall contain only static definitions.
+@sealed
+abstract class ElectroAssistThemes {
+  /// The theme that is preffered by the app unless, overriden by user
+  /// preferred theme.
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.blueGrey.shade50,
+    shadowColor: Colors.blueGrey.shade50.withOpacity(0.5),
+    textTheme: TextTheme(
+      subtitle1: TextStyle(
+        color: Colors.blueGrey.shade700,
+      ),
+      caption: TextStyle(
+        color: Colors.blueGrey.shade400,
+      ),
+      headline1: TextStyle(
+        fontWeight: FontWeight.w900,
+        letterSpacing: 1.1,
+        color: Colors.blueGrey,
+        fontSize: 30,
+      ),
+    ),
+    primaryIconTheme: IconThemeData(
+      color: Colors.blueGrey,
+    ),
+  );
+
+  /// The theme that is preffered by the app unless, overriden by user
+  /// preferred theme.
+  static final darkTheme = lightTheme;
 }
--- lib/modules/resistor_color_code/route.dart
+++ lib/modules/resistor_color_code/route.dart
@@ -44,7 +44,6 @@ class _ResistorColorCodeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
